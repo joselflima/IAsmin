bba848fbb77613c21dcaba383cc91619b51d73a4
joseferreirasankhya <jose.ferreira@sankhya.com.br>
2026-05-03 13:45:26 -0300

    feat: setup inicial e configuração do Telegram

---
+++ README.md
@@ -0,0 +1,4 @@
+# IAsmin - Assessora Política Virtual
+
+Projeto de uma assessora de política com foco em acessibilidade e representatividade.
+
---
+++ src/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file", err)
+	}
+
+	botToken := os.Getenv("TELEGRAM_BOT_TOKEN")
+	if botToken == "" {
+		log.Fatal("TELEGRAM_BOT_TOKEN not set")
+	}
+
+	bot, err := tgbotapi.NewBotAPI(botToken)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	updateConfig := tgbotapi.NewUpdate(0)
+	updateConfig.Timeout = 60
+	updates := bot.GetUpdatesChan(updateConfig)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		// For now, echo the text message on private chats and ignore everything else
+		if update.Message.Chat.Type == "private" {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			if _, err := bot.Send(msg); err != nil {
+				log.Println(err)
+			}
+		}
+	}
+}
